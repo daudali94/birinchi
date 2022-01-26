65d7c614e7f7272081899936905e5dd4d539a0d6
daudali94 <dnishanbai@gmail.com>
2022-01-26 21:00:54 +0600

    commit two

---
+++ src/com/company/Main.java
@@ -0,0 +1,48 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+       // Scanner scanner = new Scanner(System.in);
+//        int tak = 1;
+//        int tak1 = 20;
+//
+//        while (tak<tak1){
+//            tak++;
+//            if (tak % 2 == 1){
+//                System.out.println(tak);
+//            }
+//        }
+//        int a = 1;
+//        int b = 512;
+//        while (a<512){
+//            a*=2;
+//            System.out.println(a + " ");
+//        }
+//        Scanner sc = new Scanner(System.in);
+//        int a = sc.nextInt();
+//        int shetchik = 0;
+//        for (int i = a; i > 0; i-=10 ){
+//            shetchik++;
+//            System.out.println(shetchik);
+//        }
+        String[] massiv = new String[12];
+        massiv[0] = "Salam";
+        massiv[1] = "Kandai";
+        massiv[2] = "Ishterin";
+        massiv[3] = "Jakshyby";
+        massiv[4] = "Ya";
+        massiv[5] = "q";
+        massiv[6] = "w";
+        massiv[7] = "e";
+        massiv[8] = "r";
+        massiv[9] = "t";
+        massiv[10] = "y";
+        massiv[11] = "u";
+        for (int i=0; i<massiv.length; i++){
+
+        }System.out.println(massiv.length-1);
+    }
+}
